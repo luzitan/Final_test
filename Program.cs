feab7fa8990700e8f157bb6f77b1123d1edf89c0
Vera Bylinskaia <iagyar@list.ru>
2024-04-21 18:01:17 +0300

    Посчитали какой длины нужно создавать новый массив

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿string [] array = new string [] {"Hellow", "2",  "world", ":-)"};
+int count_elem = 0;
+
+foreach(string str in array){
+    if (str.Length <= 3){
+    count_elem ++;
+    }
+}
+// Console.WriteLine(count_elem);
 
 for (int i = 0; i < array.Length; i++){
     if (array[i].Length <= 3){
